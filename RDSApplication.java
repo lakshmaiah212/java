2f597168b1962ae1175b01a8817a65dc11e9dc8e
lakshmaiah212 <54611858+lakshmaiah212@users.noreply.github.com>
2019-10-21 12:59:12 +0530

    Add files via upload

---
+++ RDSApplication.java
@@ -0,0 +1,12 @@
+package com.rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RDSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RDSApplication.class, args);
+	}
+}
